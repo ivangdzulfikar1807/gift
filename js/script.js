f984ca14a47eba4d698eba98e0f4a343e56735b8
ivangdzulfikar1807 <155139742+ivangdzulfikar1807@users.noreply.github.com>
2023-12-29 14:26:43 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+
+console.log("Untuk Kamila.");
+console.log("Mila, Ivank Tau kita baru 1 minggu kenal, itupun belum pernah ketemu langsung");
+console.log("jangankan ketemu, VC aja cuma sekali itu juga pas Mila nge-VC si Marfa ahaha,");
+console.log("tapi, walaupun cuman chat aja kamu berhasil buat Ivank nyaman mil");
+console.log("jujur, Ivank suka sama Mila,");
+console.log("So, would you be my Girlfriend Mil??");
+
